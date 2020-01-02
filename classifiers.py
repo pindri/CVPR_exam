ecdb91d1c6f7d377d922549ee8d3d8a8d661778f
pindri <patrick.indri@outlook.it>
2020-01-02 13:22:25 +0100

    Completed point 8

--- classifiers.py
+++ classifiers.py
@@ -81,6 +81,12 @@ def chi_2_distance(hist_1, hist_2):
     return distance
 
 
+def earth_mover_distance(hist_1, hist_2):
+    
+    return wasserstein_distance(hist_1, hist_2)
+    
+
+
 def gaussian_kernel(hist_1, hist_2, dist_function, sigma = 0.1):
     distance = dist_function(hist_1, hist_2)
     return np.exp( - distance / (2 * sigma ** 2 ))
@@ -104,8 +110,14 @@ from sklearn.multiclass import OneVsRestClassifier
 
 def gaussian_SVM_classifier(train_df, test_df,
                             kernel_function = gaussian_kernel,
-                            dist_function = chi_2_distance):
+                            dist = 'chi'):
     
+    if dist == 'chi':
+        dist_function = chi_2_distance
+    elif dist == 'emd':
+        dist_function = earth_mover_distance
+    else:
+        raise ValueError("Unvalid distance. Use 'chi' or 'emd'")
     
     true_labels = test_df['label'].to_numpy()
     predicted_labels = []
@@ -130,3 +142,64 @@ def gaussian_SVM_classifier(train_df, test_df,
     predicted_labels = clf.predict(prediction_gram)
     
     return true_labels, np.array(predicted_labels)
+
+
+
+def hamming_distance(arr_1, arr_2):
+    """Calculate the Hamming distance between two bit strings"""
+    assert (len(arr_1) == len(arr_2)), "Arrays must have the same length."
+    return sum(c1 != c2 for c1, c2 in zip(arr_1, arr_2))
+
+
+def ecoc_classifier(train_df, test_df, n_classifiers = 25):
+    """
+    TBD
+    """
+    
+    true_labels = test_df['label'].to_numpy()
+    predicted_labels = []
+    
+    # Extract labels.
+    train_labels = train_df['label'].to_numpy()
+    classes = train_df['label'].unique()
+
+    # Extract histograms, (n_samples, n_feature) numpy array.
+    histo = np.asarray(train_df['histogram'].tolist())
+
+    # Build randomly generated coding matrix and the classifiers.
+    coding_matrix = np.random.choice([0, 1],
+                                     size = (len(classes), n_classifiers))
+    clf = [SVC() for _ in range(n_classifiers)]
+    
+    
+    # Fit each of the classifiers.
+    for idx, c in enumerate(clf):
+        
+        # class-code correspondences.
+        codes = coding_matrix[:, idx]
+        current_labels = classes[codes == 1]
+        
+        # 1 for current_labels occurrences, -1 elsewhere.
+        target = np.array(["1" if  label in current_labels else
+                           "0" for label in train_labels])
+        c = c.fit(histo, target)
+        
+        
+    # Make predictions.
+    for test_index, test_row in test_df.iterrows():
+        predicted_code = []
+        for idx, c in enumerate(clf):
+            # Predict code.
+            predicted_code.append(int(c.predict(test_row['histogram']
+                                                .reshape(1, -1))))
+        
+        # Compute distance with coding matrix rows.
+        distances = []
+        for row in coding_matrix:
+            distances.append(hamming_distance(row, predicted_code))
+                
+        # The predicted label: minimum hamming distance.
+        predicted_labels.append(classes[np.argmin(distances)])
+            
+            
+    return true_labels, predicted_labels
